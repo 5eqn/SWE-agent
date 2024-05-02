3a15d5d5571de5ee26ed61b4b8af1975c0ed3162
5eqn <491100866@qq.com>
2024-05-02 19:52:29 +0800

    :rocket: add run script

---
+++ run.sh
@@ -0,0 +1,4 @@
+python run.py --model_name ollama:llama3 \
+  --host_url http://10.249.8.158:11434 \
+  --data_path https://github.com/pvlib/pvlib-python/issues/1603 \
+  --config_file config/default_from_url.yaml
